8b0af798ae055058c52ec6007252ec80322e0d83
James Esh <jamesesh@eaglebusinesssoftware.com>
2021-12-06 15:27:39 -0500

    TYPE CONVERTERS

---
+++ VisionOnTheWeb/VisionOnTheWeb/PropertyValueConverter.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+
+namespace VisionOnTheWeb
+{
+    [TypeConverter(typeof(PropertyValueConverter))]
+    public class BindablePropertyValue
+    {
+        public BindablePropertyValue(object? value)
+        {
+            Value = value;
+        }
+        public object? Value { get; }
+    }
+
+    public class PropertyValueConverter : TypeConverter
+    {
+        public override bool CanConvertFrom(ITypeDescriptorContext? context, Type sourceType)
+        {
+            return true;
+        }
+
+        public override object? ConvertFrom(ITypeDescriptorContext? context, CultureInfo? culture, object value)
+        {
+            return new BindablePropertyValue(value);
+        }
+
+        public override bool CanConvertTo(ITypeDescriptorContext? context, Type? destinationType)
+        {
+            return true;
+        }
+
+        public override object? ConvertTo(ITypeDescriptorContext? context, CultureInfo? culture, object? value, Type destinationType)
+        {
+            return ((BindablePropertyValue?)value)?.Value?.ToString();
+        }
+    }
+}
---
+++ VisionOnTheWeb/VisionOnTheWeb/TypeTest.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+
+namespace VisionOnTheWeb
+{
+    public class TypeTest : INotifyPropertyChanged
+    {
+        object? myProperty;
+        public object? MyProperty
+        {
+            get => myProperty;
+            set
+            {
+                myProperty = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(MyPropertyBindable));
+            }
+        }
+
+        public BindablePropertyValue? MyPropertyBindable
+        {
+            get { return new BindablePropertyValue(MyProperty); }
+            set { MyProperty = value?.Value; OnPropertyChanged(); }
+        }
+
+        #region Notify
+
+        /// <summary>
+        /// Property Changed event
+        /// </summary>
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        /// <summary>
+        /// Fire the PropertyChanged event
+        /// </summary>
+        /// <param name="propertyName">Name of the property that changed (defaults from CallerMemberName)</param>
+        protected void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
