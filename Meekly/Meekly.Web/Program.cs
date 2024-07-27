e75e8a163cfd6778de2016385206b7dca1013e9f
James Esh <james@koblesystems.com>
2024-07-27 14:53:00 -0400

    fix caseing

--- Meekly/Meekly.Web/Program.cs
+++ Meekly/Meekly.Web/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello flying World!");
+app.MapGet("/", () => "Hello Flying World!");
 
 app.Run();
